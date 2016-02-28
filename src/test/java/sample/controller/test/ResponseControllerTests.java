2096c7aefed0e3893a1b1c1c96f6e7542c3e6195
jjosephy <jjosephy@Hotmail.com>
2016-02-27 21:59:31 -0800

    http unit test

--- src/test/java/sample/controller/test/ResponseControllerTests.java
+++ src/test/java/sample/controller/test/ResponseControllerTests.java
@@ -3,28 +3,51 @@ package sample.controller.test;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
+import static org.junit.Assert.*;
+import sample.controller.ResponseController;
+
 public class ResponseControllerTests {
 
     final String BASE_URL = "http://localhost:8080/";
 
     @Autowired
-    //private WebApplicationContext wac;
-    //private MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @Before
-    public void setup() {
-        //this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    public void setup() throws Exception {
+        ResponseController instance = new ResponseController();
+        this.mockMvc = MockMvcBuilders.standaloneSetup(instance).build();
     }
 
     @Test
-    public void testResponse() throws Exception{
-        /*
-         this.mockMvc.perform(get("/")
-                 .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
-                 .andExpect(status().isOk())
-                 .andExpect(content().contentType("application/json"));
-                 */
+    public void testController_fail_badRequest_noVersionHeader() throws Exception{
+        
+        //MockHttpServletRequestBuilder getRequest = get("/simple");
+        //ResultActions results = mockMvc.perform(getRequest);
+        //results.andExpect(status().isBadRequest());
+        //.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        //.andExpect(jsonPath("$.person.name").value("Jason"));
+        
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+                .get("/simple").accept(MediaType.APPLICATION_JSON)).andReturn();
+
+        Exception ex = result.getResolvedException();
+        if (ex == null) {
+            fail("expected exception is null");
+        }
+        
+        assertTrue(
+                "Unexpected Exception Message",
+                ex.getMessage().equals("Missing request header 'Api-Version' for method parameter of type String"));
+        assertTrue(
+                "Status Codes do not match",
+                result.getResponse().getStatus() == HttpStatus.BAD_REQUEST.value());
     }
 }
